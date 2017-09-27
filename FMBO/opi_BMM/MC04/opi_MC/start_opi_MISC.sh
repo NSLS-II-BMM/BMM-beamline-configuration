35b297c0a5fadacb8f211cfd82244c5030cc750a
Bruce Ravel <bravel@bnl.gov>
2017-09-27 17:14:43 -0400

    tweaking EDM interfaces, backup tool

--- FMBO/opi_BMM/MC04/opi_MC/start_opi_MISC.sh
+++ FMBO/opi_BMM/MC04/opi_MC/start_opi_MISC.sh
@@ -5,4 +5,4 @@ export EDMDATAFILES=/opt/epics/opi/opi_BMM/MC04/opi_MC
 #export EDMDATAFILES=/opt/epics/opi
 export EPICS_CA_MAX_ARRAY_BYTES=240000
 
-edm -eolc -x -m "P=XF:06BMA-BI,R={Diag:02-Ax:,A7=Y}Mtr,S=FMB,T={FS:02},MC=XF:06BMA-BI{MC:04}" MISC_MainPanel.edl
+edm -eolc -x -m "P=XF:06BMA-BI,R={Diag:02-Ax:,A7=Y}Mtr,S=FMB,T={Diag:02},MC=XF:06BMA-BI{MC:04}" MISC_MainPanel.edl
--- FMBO/opi_BMM/MC06/opi_MC/start_opi_MC06_FS.sh
+++ FMBO/opi_BMM/MC06/opi_MC/start_opi_MC06_FS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export EPICS_CA_AUTO_ADDR_LIST="Yes"
 export EPICS_CA_ADDR_LIST="127.0.0.1 localhost"
-export EDMDATAFILES=/opt/epics/opi/opi_BMM/MC04/opi_MC
+export EDMDATAFILES=/opt/epics/opi/opi_BMM/MC06/opi_MC
 #export EDMDATAFILES=/opt/epics/opi
 export EPICS_CA_MAX_ARRAY_BYTES=240000
 
